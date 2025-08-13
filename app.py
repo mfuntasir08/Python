6717b3622dca6029f70594fa95002a8cd5b2213a
mfuntasir08 <muntasirislamfahim18@gmail.com>
2025-08-13 12:56:45 +0600

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+# print('*' * 11)
+
+#  VARIABLE
+ 
+# name = 'John Smith'
+
+# is_new_patient = True
+# print(name,age,is_new_patient)
+
+# name = input("What is your name? ")
+# color = input("What is your favorite color? ")
+# print(name  + ' likes ' +  color)
+
+# birth_year = input("Enter your birth year: ")
+
+# age = 2025 - int(birth_year)
+
+# print(age)
+ 
+# name = input("What is your name? ")
+
+
+# weight = input("Enter your weight in lbs: ")
+# weight_kgs = float(weight) * 0.45
+# print( weight_kgs)
+
+# name = "John Smith"
+# # print(name[1:-1])
+
+# text = "My name is " f'{name} ,' + " i am " f'{age}'" year old."
+# print(text)
+
+# password = input("Enter your password: ")
+
+# if len(password) < 6:
+#     print("Password must be at least 6 characters long.")
+# else :
+#     print("Password is valid.")
+
+course = 'python for newbies'
+          
+print(course.find('e'))
