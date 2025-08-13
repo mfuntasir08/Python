460951fa5d20e2a5acb5b83e4b6c44394304e5a4
mfuntasir08 <muntasirislamfahim18@gmail.com>
2025-08-13 14:03:13 +0600

    if statements

--- app.py
+++ app.py
@@ -54,11 +54,29 @@
 
 # print("Enjoy your day!")
 
-house_price = 1000000
-has_good_credit = False
-if has_good_credit:
-  print('You need to put down 10%')
-  print("Down payment :$" + str (f'{house_price} * 0.1'))
-else:
-    print('You need to put down 20%')
-    print("Down payment :$" + str(f'{house_price * 0.2}'))
+
+#   print('You need to put down 10%')
+#   print("Down payment :$" + str (f'{house_price} * 0.1'))
+# else:
+#     print('You need to put down 20%')
+#     print("Down payment :$" + str(f'{house_price * 0.2}'))
+
+# price = 1000000
+# has_good_credit = False
+# if has_good_credit:
+#   down_payment = price * 0.1
+# else:
+#   down_payment = price * 0.2
+# print(f"Down payment: ${down_payment}")
+
+# name = 'JOHN SMITH'
+# age = 20
+# print(f"My Name Is {name} And I Am {age} Years Old.")
+ 
+name = input("Enter Your Name: ")
+if len(name) < 3 :
+  print("Name must be at least 3 characters")
+elif len(name) > 50:
+  print("Name can be a maximum of 50 characters")   
+else :
+  print("Name looks good!")
